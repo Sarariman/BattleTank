f585d05904a10ac053171af25817e3ae61de3612
Tweon\Tweon-PC <sculpting@yahoo.com>
2018-09-10 20:05:26 +0700

    72_Destroy Actor Timer

--- BattleTank/Source/BattleTank/Public/Projectile.h
+++ BattleTank/Source/BattleTank/Public/Projectile.h
@@ -29,6 +29,11 @@ public:
 private:
 	UProjectileMovementComponent * ProjectileMovement;
 
+	void OnTimerExpire();
+
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	float DestroyDelay = 10.f;
+
 	UFUNCTION()
 	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
 
